558ef3e0981ac6f314d11b77dacb3010a249e571
nikolas <nikosnick13@live.com>
2026-01-02 13:27:07 +0200

    feat: add a point class

---
+++ src/gr/aueb/cf/cf9/cha14/Point.java
@@ -0,0 +1,34 @@
+package gr.aueb.cf.cf9.cha14;
+
+public class Point {
+
+    private double x;
+
+    public  Point(){}
+
+    public Point(int x){
+
+        this.x = x;
+    }
+
+    public void setX(int x) {
+
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public void movePlusOne(){
+        x += 1;
+    }
+
+    private void movePlus10(){
+        x += 10;
+    }
+
+    private void reset(){
+        x = o;
+    }
+
+}
