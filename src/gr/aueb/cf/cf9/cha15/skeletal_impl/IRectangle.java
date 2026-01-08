db7faa6be777e197883d6dbe559d8e42b1e0d68c
nikolas <nikosnick13@live.com>
2026-01-08 14:38:25 +0200

    feat: add interface class for rectangle services (Public API)

---
+++ src/gr/aueb/cf/cf9/cha15/skeletal_impl/IRectangle.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.cf9.cha15.skeletal_impl;
+
+/**
+ *  Public API
+ */
+public interface IRectangle {
+
+    double  getArea();
+    double getPerimeter();
+    boolean isSquare();
+}
