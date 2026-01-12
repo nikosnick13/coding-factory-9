4bd91de28721606e93d3150b670cb4540ceacbdf
nikolas <nikosnick13@live.com>
2026-01-12 12:44:24 +0200

    feat: move to another package

---
+++ src/gr/aueb/cf/cf9/cha16/Book.java
@@ -0,0 +1,64 @@
+package gr.aueb.cf.cf9.cha16;
+
+import java.awt.*;
+
+public class Book {
+
+    private final long id;
+    private final String title;
+    private final String author;
+    private final String isbn;
+
+    private Book(Builder builder){
+
+        this.id = builder.id;
+        this.title = builder.title;
+        this.author = builder.author;
+        this.isbn = builder.isbn;
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public static class Builder{
+
+        private final long id;
+        private String title = "";
+        private String author = "";
+        private final String isbn;
+
+        // Fluent Setter - returns the Builder object - chaining
+        public Builder(long id, String isbn){
+            this.id = id;
+            this.isbn = isbn;
+        }
+
+        public Builder title(String title){
+            this.title = title;
+            return this;
+        }
+
+        public Builder author(String author){
+            this.author = author;
+            return  this;
+        }
+
+        public Book build(){
+            return new Book(this);
+        }
+    }
+}
---
+++ src/gr/aueb/cf/cf9/cha16/Main.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.cf9.cha16;
+
+public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book.Builder(12l,"123123").build();
+        Book book2 = new Book.Builder(13L, "123432")
+                .author("Nikos")
+                .build();
+        Book book3 = new Book.Builder(14L, "57898")
+                .title("Lord of the rings")
+                .author("Tolkin")
+                .build();
+
+
+    }
+}
