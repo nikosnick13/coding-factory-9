a93d62f8964147558e797e08a977fb753358946f
nikolas <nikosnick13@live.com>
2026-01-16 11:05:17 +0200

    feat: add comments

---
+++ src/gr/aueb/cf/cf9/cha18/BankApp/DAO/AccountDAOImpl.java
@@ -0,0 +1,57 @@
+package gr.aueb.cf.cf9.cha18.BankApp.DAO;
+
+import gr.aueb.cf.cf9.cha18.BankApp.Models.Account;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * In-memory implementation of IAccountDAO.
+ *
+ * Stores Account objects in a List and provides basic
+ * CRUD operations without using a database.
+ */
+public class AccountDAOImpl implements IAccountDAO{
+
+    List<Account> accounts = new ArrayList<>();
+
+    @Override
+    public Account saveOrUpdate(Account account) {
+
+        int index = accounts.indexOf(account); // προιποθεση override equals
+
+        if(index == -1){              // αν δεν υπάρχει
+            accounts.add(account);    //τοτε κάνε insert
+            return account;
+        }
+        accounts.set(index,account);  //τοτε κανε update
+        return accounts.get(index);
+    }
+
+    @Override
+    public void remove(String iban) {
+        accounts.removeIf( account -> account.getIban().equals(iban));
+    }
+
+    @Override
+    public Optional<Account> getByIban(String iban) {
+        return accounts.stream()
+                .filter( account -> account.getIban().equals(iban))
+                .findFirst();
+    }
+
+    @Override
+    public List<Account> getAllAccounts() {
+        //return  new ArrayList<>(accounts);
+        return Collections.unmodifiableList(accounts);
+        //return  List.copyOf(accounts);
+    }
+
+    @Override
+    public boolean isAccountExist(String iban) {
+        return  accounts.stream()
+                .anyMatch( account -> account.getIban().equals(iban));
+    }
+}
---
+++ src/gr/aueb/cf/cf9/cha18/BankApp/DAO/IAccountDAO.java
@@ -0,0 +1,27 @@
+package gr.aueb.cf.cf9.cha18.BankApp.DAO;
+
+import gr.aueb.cf.cf9.cha18.BankApp.Models.Account;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Data Access Object (DAO) interface for Account entities.
+ *
+ * Provides CRUD operations and query methods for managing
+ * Account persistence.
+ */
+public interface IAccountDAO {
+
+    Account saveOrUpdate(Account account);
+
+    void remove(String iban);
+
+   Optional<Account> getByIban(String iban);
+
+   List<Account> getAllAccounts();
+
+   //Queries
+    boolean isAccountExist(String iban);
+
+}
