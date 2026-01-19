80bf1e3b2022fa02b1613d04c43804841d7d0629
nikolas <nikosnick13@live.com>
2026-01-19 11:24:08 +0200

    feat: services

--- src/gr/aueb/cf/cf9/cha18/BankApp/core/exceptions/InsufficentBalanceException.java
+++
@@ -1,9 +0,0 @@
-package gr.aueb.cf.cf9.cha18.BankApp.core.exceptions;
-
-public class InsufficentBalanceException extends Exception{
-
-    public InsufficentBalanceException(String message){
-        super(message);
-    }
-
-}
---
+++ src/gr/aueb/cf/cf9/cha18/BankApp/core/exceptions/InsufficientBalanceException.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.cf9.cha18.BankApp.core.exceptions;
+
+public class InsufficientBalanceException extends Exception{
+
+    public InsufficientBalanceException(String message){
+        super(message);
+    }
+
+}
