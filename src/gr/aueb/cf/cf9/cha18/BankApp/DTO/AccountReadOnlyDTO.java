9a0ce52eade018045c7d3f4206b94665365833a2
nikolas <nikosnick13@live.com>
2026-01-20 11:16:10 +0200

    feat: add toString @Override

--- src/gr/aueb/cf/cf9/cha18/BankApp/DTO/AccountReadOnlyDTO.java
+++ src/gr/aueb/cf/cf9/cha18/BankApp/DTO/AccountReadOnlyDTO.java
@@ -3,4 +3,9 @@ package gr.aueb.cf.cf9.cha18.BankApp.DTO;
 import java.math.BigDecimal;
 
 public record AccountReadOnlyDTO(String iban, BigDecimal balance) {
+
+    @Override
+    public String toString() {
+        return "IBAN: " + iban + ", Balance: " + balance;
+    }
 }
