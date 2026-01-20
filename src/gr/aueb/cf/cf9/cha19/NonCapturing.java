2f82d793ec978d575fc6052e58c37fc0ec16a282
nikolas <nikosnick13@live.com>
2026-01-20 14:57:05 +0200

    feat: add RegEx

--- src/gr/aueb/cf/cf9/cha19/NonCapturing.java
+++ src/gr/aueb/cf/cf9/cha19/NonCapturing.java
@@ -1,8 +1,22 @@
 package gr.aueb.cf.cf9.cha19;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class NonCapturing {
 
-    public static void main(String[] args) {
+    public  static void main(String[] args) {
+        String s = "HelloWorld HelloWorld";
 
+        Pattern pattern  = Pattern.compile("(Hello)(?:World)");
+        Matcher matcher = pattern.matcher(s);
+
+        while (matcher.find()) {
+//            System.out.println(matcher.group(0));
+            for (int i = 1; i <= matcher.groupCount(); i++) {
+                System.out.print(matcher.group(i) + " ");
+            }
+            System.out.println();
+        }
     }
 }
