4dab0e91eb562409e63faad5a9425faafb1ff2be
nikolas <nikosnick13@live.com>
2026-01-20 11:14:19 +0200

    fix: typo from AccountNotFountException to AccountNotFoundException

--- src/gr/aueb/cf/cf9/cha18/BankApp/core/exceptions/AccountNotFountException.java
+++
@@ -1,8 +0,0 @@
-package gr.aueb.cf.cf9.cha18.BankApp.core.exceptions;
-
-public class AccountNotFountException extends Exception{
-
-    public AccountNotFountException(String message){
-        super(message);
-    }
-}
